408c348bba7cd210813540cfd240f1166305bf2a
Andrey Madzhuha <Andrey.mad.87@gmail.com>
2019-10-12 14:54:54 +0300

    change branch

--- src/services/services.js
+++ src/services/services.js
@@ -11,26 +11,105 @@ export default {
       throw new Error("Error");
     }
   },
-  async getUserCategories() {
-    try {
-      const getCategories = await axios.post("/auth/login", {
-        email: "test9@gmail.com",
-        password: "qwerty"
-      });
 
-      return getCategories.data.categories;
+  async getAdId(id) {
+    try {
+      const AdId = await axios.get(`/ads/${id}`);
+      return AdId.data;
     } catch (error) {
       throw new Error("Error");
     }
   },
 
-  async getByCategories(inputCategory) {
+  async getAdLimit(limit, pageNumber = 1) {
     try {
-      const categoriesQuery = axios.get(
-        `/ads/all?category=${inputCategory}&page=1`
+      const adLimit = await axios.get(
+        `/ads/all?limit=${limit}&page=${pageNumber}`
       );
-      const filteredAds = categoriesQuery.then(data => data.data.ads.docs);
-      return filteredAds;
+      return adLimit.data;
+    } catch (error) {
+      throw new Error("Error");
+    }
+  },
+
+  async getAdByCategory(category, pageNumber = 1) {
+    try {
+      const AdByCategory = await axios.get(
+        `/ads/all?category=${category}$page=${pageNumber}`
+      );
+      return AdByCategory.data;
+    } catch (error) {
+      throw new Error("Error");
+    }
+  },
+
+  async userCreate(userData) {
+    try {
+      const addUser = await axios.post(`/auth/register`, {
+        email: userData.email,
+        password: userData.password,
+        name: userData.name
+      });
+      return addUser.data;
+    } catch (error) {
+      throw new Error("Error");
+    }
+  },
+
+  async userLogin(userData) {
+    try {
+      const user = await axios.post(`/auth/login`, {
+        email: userData.email,
+        password: userData.password
+      });
+      return user.data;
+    } catch (error) {
+      throw new Error("Error");
+    }
+  },
+
+  async userLogout(userData, token) {
+    try {
+      const user = await axios({
+        method: "post",
+        url: `/auth/logout`,
+        headers: {
+          Authorization: token
+        },
+        data: userData
+      });
+      return user.data;
+    } catch (error) {
+      throw new Error("Error");
+    }
+  },
+
+  async submitUserAd(userAd, token) {
+    try {
+      const user = await axios({
+        method: "post",
+        url: `/ads`,
+        headers: {
+          Authorization: token
+        },
+        data: userAd
+      });
+      return user.data;
+    } catch (error) {
+      throw new Error("Error");
+    }
+  },
+
+  async deleteUserAd(adId, token) {
+    try {
+      const user = await axios({
+        method: "delete",
+        url: `/ads/${adId}`,
+        headers: {
+          Authorization: token
+        }
+      });
+      return user.data;
     } catch (error) {
       throw new Error("Error");
     }
