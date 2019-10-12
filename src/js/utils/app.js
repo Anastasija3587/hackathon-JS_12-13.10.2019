fc1f50791cf7aae3ed5e120856d55457fda9b3ec
Andrey Madzhuha <Andrey.mad.87@gmail.com>
2019-10-12 13:53:23 +0300

    some code

---
+++ src/js/utils/app.js
@@ -0,0 +1,14 @@
+import services from "../../services/services";
+let categoriesWrapper = document.querySelector(".categories_wrapper");
+
+function defaultCategories() {
+  services.getUserCategories().then(data => console.log(data));
+}
+
+function getFilteredAds({ target }) {
+  if (target.dataset.id) {
+    services.getByCategories(target.dataset.id).then(console.log);
+  }
+}
+
+categoriesWrapper.addEventListener("click", getFilteredAds);
--- src/services/services.js
+++ src/services/services.js
@@ -10,5 +10,29 @@ export default {
     } catch (error) {
       throw new Error("Error");
     }
+  },
+  async getUserCategories() {
+    try {
+      const getCategories = await axios.post("/auth/login", {
+        email: "test9@gmail.com",
+        password: "qwerty"
+      });
+
+      return getCategories.data.categories;
+    } catch (error) {
+      throw new Error("Error");
+    }
+  },
+
+  async getByCategories(inputCategory) {
+    try {
+      const categoriesQuery = axios.get(
+        `/ads/all?category=${inputCategory}&page=1`
+      );
+      const filteredAds = categoriesQuery.then(data => data.data.ads.docs);
+      return filteredAds;
+    } catch (error) {
+      throw new Error("Error");
+    }
   }
 };
