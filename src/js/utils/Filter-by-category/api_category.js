52bd7a7aad264ade2a4052e62c1a0b663944d044
Andrey Madzhuha <Andrey.mad.87@gmail.com>
2019-10-13 11:17:35 +0300

    render cards done

--- src/js/utils/Filter-by-category/api_category.js
+++ src/js/utils/Filter-by-category/api_category.js
@@ -1,10 +1,10 @@
 import services from "../../../services/services";
-import renderFunc from './renderCards'
+// import renderFunc from './renderCards'
 import template from "../../../templates/category.hbs";
 const refs = {
   container: document.querySelector(".categories__list")
 };
-
+renderCards("all");
 services.getAdByCategory(1).then(data => {
   const onlyContentCategory = data.ads.categories
     .map(
@@ -18,25 +18,46 @@ services.getAdByCategory(1).then(data => {
   let htmlToRender = onlyContentCategory + clearBtn;
   refs.container.insertAdjacentHTML("beforeend", htmlToRender);
 
-
   const btn = document.querySelectorAll(".categories__list-btn");
   const clearFilterBtn = document.querySelector(
     ".categories__list-item_clear_filter"
   );
   const handleClickCategory = e => {
+    //удаляем клас active и добавляем в активную кнопку
     btn.forEach(el => el.classList.remove("active"));
     e.target.classList.add("active");
-    services.chooseCategory(e.target.id);
-    services
-      .getAdByCategory(services.giveCategory())
-      .then(data => console.log(data));
+    services.chooseCategory(e.target.closest("li").dataset.id);
+
+    services.getAdByCategory(services.giveCategory()).then(data => {
+      let arrToRender = data.ads.docs.map(el => {
+        return template(el);
+      });
+
+      let container = document.querySelector(".cotainer_allCategory");
+      container.innerHTML = "";
+      container.insertAdjacentHTML("beforeend", arrToRender);
+    });
   };
 
   const clearFilter = () => {
     btn.forEach(el => el.classList.remove("active"));
-    return services.getAd().then(data => console.log(data));
+    renderCards("all");
   };
-  
+
   clearFilterBtn.addEventListener("click", clearFilter);
   btn.forEach(elem => elem.addEventListener("click", handleClickCategory));
 });
+
+//функция отрисовки карточек в контейнере
+
+function renderCards(id) {
+  let container = document.querySelector(".cotainer_allCategory");
+  container.innerHTML = "";
+  services.getAdId(id).then(data => {
+    let arrToRender = data.ads.docs.map(el => {
+      return template(el);
+    });
+
+    container.insertAdjacentHTML("beforeend", arrToRender);
+  });
+}
--- src/js/utils/Filter-by-category/renderCards.js
+++ src/js/utils/Filter-by-category/renderCards.js
@@ -1,15 +1,10 @@
-import template from "../../../templates/category.hbs";
+// import template from "../../../templates/category.hbs";
+// import services from "../../../services/services";
 
-import services from "../../../services/services";
-
-console.log("hello");
-
- export default services.getAd().then(data => {
-  console.log(data);
-  let arrToRender = data.ads.docs.map(el => {
-
-    return template(el);
-  });
-  let container = document.querySelector(".cotainer_allCategory");
-  container.insertAdjacentHTML("beforeend", arrToRender);
-});
+// export default services.getAdId(id).then(data => {
+//   let arrToRender = data.ads.docs.map(el => {
+//     return template(el);
+//   });
+//   let container = document.querySelector(".cotainer_allCategory");
+//   container.insertAdjacentHTML("beforeend", arrToRender);
+// });
