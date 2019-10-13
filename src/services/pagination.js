56820a01d4e51c9551cd79e0ae7d658619af1749
Artem2313 <huselnykov@gmail.com>
2019-10-13 14:36:07 +0300

    wait for search

--- src/services/pagination.js
+++ src/services/pagination.js
@@ -1,36 +1,9 @@
 import services from '../services/services'
 
-// export default {refs = {
-//         next: document.querySelector(".next"),
-//         previous: document.querySelector(".previous"),
-//         container: document.querySelector(".container"),
-//         search: document.querySelector(".search")     
-// },
-//   image: [],
-//   searchQuery: "" ? "" : "javaScript",
-//   page: 1,  
-//   api: `https://dash-ads.goit.co.ua/api/v1`,
-//   fetchPost: function(value) {
-//     console.log("value", value);
-//     console.log("searchQuery", this.searchQuery);
-//     console.log(this.page);
-//     this.nextPage();
-//     return axios.get(
-//       `${this.api}q=${value}&page=${this.page}`
-//     );
-// },
-// nextPage() {
-//     this.page += 1;
-//   },
-// prevPage() {
-//     this.page -= 1;
-//   },
-// }
-
 console.log('fjfh')
 const nextPage = document.querySelector('.next')
 const container = document.querySelector('.container')
-const search = document.querySelector('.search')
+// const search = document.querySelector('.search')
 
 const allAd = () => {
     services.getAdLimit().then(data => {
@@ -50,24 +23,24 @@ const next = () => {
     )
 }
 
-const getQurty = evt => {
-    services.defaultPage();
-    console.log(evt.target.value);
-    services.refs.container.innerHTML = "";
-    services.getValue(evt.target.value);
-    services.fetchNews(evt.target.value).then(data => {
-      console.log(data.data);
-      const renderToHtml = data.data.articles
-        .map(elem => `<img width='300' src='${elem.urlToImage}'/>`)
-        .join("");
+// const getQurty = evt => {
+//     services.defaultPage();
+//     console.log(evt.target.value);
+//     services.refs.container.innerHTML = "";
+//     services.getValue(evt.target.value);
+//     services.fetchNews(evt.target.value).then(data => {
+//       console.log(data.data);
+//       const renderToHtml = data.data.articles
+//         .map(elem => `<img width='300' src='${elem.urlToImage}'/>`)
+//         .join("");
   
-      services.refs.container.insertAdjacentHTML("beforeend", renderToHtml);
-    });
-  };
+//       services.refs.container.insertAdjacentHTML("beforeend", renderToHtml);
+//     });
+//   };
 
 
 
 
 nextPage.addEventListener('click', next);
 
-services.refs.qurty.addEventListener("change", getQurty);
+
