5e9ac46ef3db512950e8adc1431b0c435059a304
Vladyslav <yarovoyvlad437@gmail.com>
2019-10-13 14:06:18 +0300

    1

--- src/js/utils/Filter-by-category/api_category.js
+++ src/js/utils/Filter-by-category/api_category.js
@@ -1,68 +1,3 @@
-<<<<<<< HEAD
-import services from "../../../services/services";
-// import renderFunc from './renderCards'
-import template from "../../../templates/category.hbs";
-const refs = {
-  container: document.querySelector(".categories__list")
-};
-renderCards("all");
-services.getAdByCategory(1).then(data => {
-  const onlyContentCategory = data.ads.categories
-    .map(
-      elem =>
-        `<li class="categories__list-item" data-id="${elem._id}">   
-        <button class="categories__list-btn">"${elem.category}"</button>
-       </li> `
-    )
-    .join("");
-  let clearBtn = `<button class="categories__list-item_clear_filter">Очистить фильтр</button>`;
-  let htmlToRender = onlyContentCategory + clearBtn;
-  refs.container.insertAdjacentHTML("beforeend", htmlToRender);
-
-  const btn = document.querySelectorAll(".categories__list-btn");
-  const clearFilterBtn = document.querySelector(
-    ".categories__list-item_clear_filter"
-  );
-  const handleClickCategory = e => {
-    //удаляем клас active и добавляем в активную кнопку
-    btn.forEach(el => el.classList.remove("active"));
-    e.target.classList.add("active");
-    services.chooseCategory(e.target.closest("li").dataset.id);
-
-    services.getAdByCategory(services.giveCategory()).then(data => {
-      let arrToRender = data.ads.docs.map(el => {
-        return template(el);
-      });
-
-      let container = document.querySelector(".cotainer_allCategory");
-      container.innerHTML = "";
-      container.insertAdjacentHTML("beforeend", arrToRender.join(""));
-    });
-  };
-
-  const clearFilter = () => {
-    btn.forEach(el => el.classList.remove("active"));
-    renderCards("all");
-  };
-
-  clearFilterBtn.addEventListener("click", clearFilter);
-  btn.forEach(elem => elem.addEventListener("click", handleClickCategory));
-});
-
-//функция отрисовки карточек в контейнере
-
-function renderCards(id) {
-  let container = document.querySelector(".cotainer_allCategory");
-  container.innerHTML = "";
-  services.getAdId(id).then(data => {
-    let arrToRender = data.ads.docs.map(el => {
-      return template(el);
-    });
-
-    container.insertAdjacentHTML("beforeend", arrToRender.join(""));
-  });
-}
-=======
 import services from "../../../services/services";
 import template from "../../../templates/category.hbs";
 const refs = {
@@ -139,4 +74,3 @@ function renderCards(id) {
     container.insertAdjacentHTML("beforeend", arrToRender.join(""));
   });
 }
->>>>>>> dev
